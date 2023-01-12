9434285e13530e301e7724140b96d996b711d38f
Kosmas_Stamos <kossmasstamoss@gmail.com>
2023-01-12 17:41:53 +0200

    Session-04 Assignments

---
+++ Session-04/Session-04/Session-04/ExcOne.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_04
+{
+    internal class ExcOne
+    {
+     public void Greeting()
+        {
+            string name = "Kosmas";
+            Console.WriteLine(" Hello " + name + "\n");
+        }
+
+    }
+
+}

bb7d245d1db0518095f6308e9ff556d08dfc589c
Kosmas_Stamos <kossmasstamoss@gmail.com>
2023-01-12 18:27:58 +0200

    One last time....forgot to stage  2 files

---
+++ Session-04/Session-04/ExcTwo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_04
+{
+    internal class ExcTwo
+    {
+
+        public int x = 7, y = 2;
+        public int Addition()
+        {
+            int sum = x + y;
+            return sum;
+        }
+
+        public float Division()
+        {
+            float res = (float)x / (float)y;
+            return res;
+        }
+    }
+}

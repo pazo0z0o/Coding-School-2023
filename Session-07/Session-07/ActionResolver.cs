675345d6b1e1c124d39d9f689b71f77dcce268be
Kosmas_Stamos <kossmasstamoss@gmail.com>
2023-01-17 19:09:36 +0200

    Sigh

--- Session-07/Session-07/ActionResolver.cs
+++ Session-07/Session-07/ActionResolver.cs
@@ -18,27 +18,30 @@ namespace Session_07
 
         public ActionResolver(Guid requestId, Guid responseId, string output) : base(requestId, responseId, output) { }
 
-        Actions act = new Actions();
+        //Actions act = new Actions();
 
         //Methods
         public ActionResponse Execute(ActionRequest request)
-        {
+        {   ActionResponse response = null;
             switch (ActionEnum)
             {
                 case Action.Convert:
-                    act.Converter(Input);
+                    BinConverter act = new BinConverter();
+                   act.Action(Input);
                     break;
                 case Action.Uppercase:
-                    act.UpperCase(Input);
+                    UpperCase act1= new UpperCase();
+                    act1.Action(Input);
 
                     break;
                 case Action.Reverse:
-                    act.Reversal(Input);
+                    Reversal act2 = new Reversal();
+                    act2.Action(Input);
                     break;
-                default:
+                
                    
 
-                   return null;
+                   return response;
             }
 
 
--- Session-07/Session-07/Actions.cs
+++ Session-07/Session-07/Actions.cs
@@ -6,69 +6,98 @@ using System.Threading.Tasks;
 
 namespace Session_07
 {
-    internal class Actions
+    public class Actions
     {
         public string Input { get; set; }
-        
-        public string Output { get; set; }  
-        
-        public Actions() { }
+        public string Output { get; set; }
 
+        public Actions() { } //empty constr
+        public Actions(string input)
+        { Input = input; }
 
-
-        public   int Converter(string input)
+        //method to be virtual
+        public virtual string Action(string Input)
         {
-            int n, i,outputBin=0;
-            int[] a = new int[10];
-
-            if (int.TryParse(input, out n))
-            {
-                for (i = 0; n > 0; i++)
-                {
-                    a[i] = n % 2;
-                    n = n / 2;
-                }
-                for ( i = 0; i < a.Length; i++)
-                { 
-                     outputBin += a[i] * Convert.ToInt32(Math.Pow(10, a.Length - i - 1));
-                }
-
-            }
-            return outputBin;
+            return Output;
         }
-          public  string UpperCase(string input)
+    }
+
+        public class BinConverter : Actions
+        {
+            public BinConverter() { }
+            public BinConverter(string input) : base(input) { }
+
+            public override string Action(string input)
             {
-            string[] words = input.Split(new[] { " " }, StringSplitOptions.None);
-            string UpperStr = "";
-            int ctr = 0;
-            foreach (String s in words)
-            {
-                if (s.Length > ctr)
+                int n, i, outputBin = 0;
+                int[] a = new int[10];
+
+                if (int.TryParse(input, out n))
                 {
-                    UpperStr = s;
-                    ctr = s.Length;
+                    for (i = 0; n > 0; i++)
+                    {
+                        a[i] = n % 2;
+                        n = n / 2;
+                    }
+                    for (i = 0; i < a.Length; i++)
+                    {
+                        outputBin += a[i] * Convert.ToInt32(Math.Pow(10, a.Length - i - 1));
+                    }
+
                 }
-
+                return outputBin.ToString();
             }
 
-            return UpperStr; 
-            }
-
-             public  string Reversal(string input) 
-             {
-            
-            char[] arrRevrse = input.ToCharArray();
-            string ans = " ";
-            
-            if(input is string) 
-            {
-                for (int i = arrRevrse.Length - 1; i >= 0; i--)
-                {
-                    ans += arrRevrse[i];
-                }
-            }
-            return ans;
         }
+
+        public class UpperCase : Actions
+        {
+            public UpperCase() { }
+            public UpperCase(string input) : base(input) { }
+            public override string Action(string input)
+            {
+                string[] words = input.Split(new[] { " " }, StringSplitOptions.None);
+                string UpperStr = "";
+                int ctr = 0;
+                foreach (String s in words)
+                {
+                    if (s.Length > ctr)
+                    {
+                        UpperStr = s;
+                        ctr = s.Length;
+                    }
+
+                }
+
+                return UpperStr;
+            }
+
+
+        }
+
+        public class Reversal : Actions
+        {
+            public Reversal() { }
+            public Reversal(string input) : base(input) { }
+            public override string Action(string input)
+            {
+                char[] arrRevrse = input.ToCharArray();
+                string ans = " ";
+
+                if (input is string)
+                {
+                    for (int i = arrRevrse.Length - 1; i >= 0; i--)
+                    {
+                        ans += arrRevrse[i];
+                    }
+                }
+                return ans;
+
+            }
+
+
+        }
+
 
             
                
@@ -83,5 +112,5 @@ namespace Session_07
 
 
 
-    }
+    
 }

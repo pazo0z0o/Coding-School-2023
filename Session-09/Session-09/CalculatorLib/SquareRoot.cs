33de90f88a7fb81b1139dcb0cd4047f3d6d069b3
Kosmas_Stamos <kossmasstamoss@gmail.com>
2023-01-20 09:29:36 +0200

    Split the calculation portion into different files

---
+++ Session-09/Session-09/CalculatorLib/SquareRoot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLib
+{
+    public class SquareRoot : Operations
+    {//have to implement it since I wanted to try and abstract
+        public override decimal Op(decimal? x, decimal? y) { return 0; }
+        public override decimal Op(decimal? x)
+        {
+            decimal ret = 0;
+
+            if (x != null && x > 0)
+            {
+                ret = (decimal)Math.Sqrt((double)x.Value);
+            }
+
+            return ret;
+        }
+
+    }
+}
---
+++ Session-09/Session-09/CalculatorLib/Subtraction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLib
+{
+    public class Subtraction : Operations
+    {
+        //have to implement cause of abstract class base
+        public override decimal Op(decimal? x) { return 0; }
+        public override decimal Op(decimal? x, decimal? y)
+        {
+            decimal ret = 0;
+            if (x != null && y != null)
+            {
+                ret = x.Value - y.Value;
+            }
+            return ret;
+        }
+    }
+}

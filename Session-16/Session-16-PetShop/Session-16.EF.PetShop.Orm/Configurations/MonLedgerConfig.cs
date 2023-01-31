553212825414ee2691566c5f1ad924146748a02b
Kosmas_Stamos <kossmasstamoss@gmail.com>
2023-01-31 13:41:23 +0200

    error with identity property in PetReport and Monthly Ledger

--- Session-16/Session-16-PetShop/Session-16.EF.PetShop.Orm/Configurations/MonLedgerConfig.cs
+++ Session-16/Session-16-PetShop/Session-16.EF.PetShop.Orm/Configurations/MonLedgerConfig.cs
@@ -14,8 +14,7 @@ namespace Session_16.EF.PetShop.Orm.Configurations
         public void Configure(EntityTypeBuilder<MonthlyLedger> builder)
         {
             builder.HasKey(todo => todo.Id); //Primary key HasKey
-                                             // builder.HasKey(todo => todo.Month);  does this count as a composite key assignment?
-            builder.Property(todo => todo.Id).ValueGeneratedOnAdd();
+            builder.Property(todo => todo.Id);
             
             builder.Property(todo => todo.Year).HasMaxLength(4).IsRequired(true);
             builder.Property(todo => todo.Month).HasMaxLength(15).IsRequired(true);

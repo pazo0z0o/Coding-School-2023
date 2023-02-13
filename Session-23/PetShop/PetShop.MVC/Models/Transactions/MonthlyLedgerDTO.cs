6eea1fee09ac5ec770e21047570145e5552e6a1d
Kosmas_Stamos <kossmasstamoss@gmail.com>
2023-02-13 04:46:46 +0200

    Im done , cant do it anymore

---
+++ Session-23/PetShop/PetShop.MVC/Models/Transactions/MonthlyLedgerDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace PetShop.MVC.Models.Transactions
+{
+    public class MonthlyLedgerDTO
+    {
+
+        List<decimal> MonthIncome { get; set; } = new List<decimal>();
+
+        List<decimal> MonthlyExpenses { get; set; } = new List<decimal>();
+
+        List<decimal> Total { get; set; } = new List<decimal>();
+
+    }
+}

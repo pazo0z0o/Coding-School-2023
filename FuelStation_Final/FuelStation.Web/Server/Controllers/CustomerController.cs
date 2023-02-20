0c855093c5a1622e296ef7286ccd29f9fc9a8c74
Kosmas_Stamos <kossmasstamoss@gmail.com>
2023-02-20 12:50:17 +0200

    EmployeeController

--- FuelStation_Final/FuelStation.Web/Server/Controllers/CustomerController.cs
+++ FuelStation_Final/FuelStation.Web/Server/Controllers/CustomerController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using FuelStation.EF.Repositories;
 using FuelStation.Model;
 using FuelStation.Web.Shared;
+using FuelStation.Web.Shared.ManagerStaffSharedDTOs;
 
 namespace FuelStation.Web.Server.Controllers
 {
@@ -10,5 +11,73 @@ namespace FuelStation.Web.Server.Controllers
     [ApiController]
     public class CustomerController : ControllerBase
     {
+        private readonly IEntityRepo<Customer> _customerRepo;
+        public CustomerController(IEntityRepo<Customer> customerRepo)
+        {
+            _customerRepo = customerRepo;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<CustomerListDTO>> Get()
+        {
+            var customerList = _customerRepo.GetAll();
+            return customerList.Select(customer => new CustomerListDTO
+            {
+                ID = customer.ID,
+                Name = customer.Name,
+                Surname = customer.Surname,
+                CardNumber = customer.CardNumber,
+            });
+        }
+
+        [HttpPost]
+        public async Task Post(CustomerCreateDTO customer)
+        {
+            var newCustomer = new Customer(customer.Name, customer.Surname, customer.CardNumber);
+            newCustomer.Transactions = new();
+            _customerRepo.Add(newCustomer);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            _customerRepo.Delete(id);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<CustomerEditDTO> GetById(int id)
+        {
+            var result = _customerRepo.GetById(id);
+            return new CustomerEditDTO
+            {
+                ID = id,
+                Name = result.Name,
+                Surname = result.Surname,
+                CardNumber = result.CardNumber
+            };
+        }
+        //Put
+        public async Task Put(CustomerEditDTO customer)
+        {
+            var itemToUpdate = _customerRepo.GetById(customer.ID);
+            itemToUpdate.ID = customer.ID;
+            itemToUpdate.Name = customer.Name;
+            itemToUpdate.Surname = customer.Surname;
+            itemToUpdate.CardNumber = customer.CardNumber;            
+            itemToUpdate.Transactions = customer.Transactions;
+            _customerRepo.Update(customer.ID, itemToUpdate);
+        }
+
+
+
+
+
+
+
+
+
+
     }
 }
--- FuelStation_Final/FuelStation.Web/Server/Controllers/EmployeeController.cs
+++ FuelStation_Final/FuelStation.Web/Server/Controllers/EmployeeController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using FuelStation.EF.Repositories;
 using FuelStation.Model;
 using FuelStation.Web.Shared;
+using FuelStation.Web.Shared.ManagerStaffSharedDTOs;
+using FuelStation.Web.Shared.ManagerOnlyDTOs;
 
 namespace FuelStation.Web.Server.Controllers
 {
@@ -18,6 +20,77 @@ namespace FuelStation.Web.Server.Controllers
         {
             _employeeRepo= employeeRepo;
         }
+
+
+        [HttpGet]
+        public async Task<IEnumerable<EmployeeListDTO>> Get()
+        {
+            var employeelist = _employeeRepo.GetAll();
+            return employeelist.Select(employee => new EmployeeListDTO
+            {
+                ID = employee.ID,
+                Name = employee.Name,
+                Surname = employee.Surname,
+                HireDateStart = employee.HireDateStart,
+                HireDateEnd = employee.HireDateEnd,
+                SallaryPerMonth= employee.SallaryPerMonth,
+                EmployeeType = employee.EmployeeType,
+            });
+        }
+
+        [HttpPost]
+        public async Task Post(EmployeeListDTO employee)
+        {
+            var newEmployee = new Employee();
+
+                newEmployee.Name = employee.Name;
+                newEmployee.Surname = employee.Surname;
+                newEmployee.HireDateStart = employee.HireDateStart;
+                newEmployee.HireDateEnd = employee.HireDateEnd;
+                newEmployee.SallaryPerMonth = employee.SallaryPerMonth;    
+                newEmployee.EmployeeType = employee.EmployeeType;
+                newEmployee.Transactions = new();
+                _employeeRepo.Add(newEmployee);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            _employeeRepo.Delete(id);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<EmployeeListDTO> GetById(int id)
+        {
+            var result = _employeeRepo.GetById(id);
+            return new EmployeeListDTO
+            {
+                ID = id,
+                Name = result.Name,
+                Surname = result.Surname,
+                HireDateStart = result.HireDateStart,
+                HireDateEnd = result.HireDateEnd,
+                SallaryPerMonth= result.SallaryPerMonth,
+                EmployeeType = result.EmployeeType
+            };
+        }
+        //Put
+        public async Task Put(EmployeeListDTO employee)
+        {
+            var itemToUpdate = _employeeRepo.GetById(employee.ID);
+            itemToUpdate.ID = employee.ID;
+            itemToUpdate.Name = employee.Name;
+            itemToUpdate.Surname = employee.Surname;
+            itemToUpdate.HireDateStart = employee.HireDateStart;
+            itemToUpdate.HireDateEnd = employee.HireDateEnd;
+            itemToUpdate.SallaryPerMonth= employee.SallaryPerMonth;
+            itemToUpdate.EmployeeType = employee.EmployeeType;
+            itemToUpdate.Transactions = employee.Transactions;
+            _employeeRepo.Update(employee.ID, itemToUpdate);
+        }
+
 
     }
 }

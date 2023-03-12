5f1013e48a7830c116f19c3e2cc693228ed6edf0
Kosmas_Stamos <kossmasstamoss@gmail.com>
2023-03-12 14:21:52 +0200

    Added the 2 new repos to be filled.

---
+++ PetShop/PetShop/PetShop.EF/Repositories/StockRepo.cs
@@ -0,0 +1,37 @@
+﻿using PetShop.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.EF.Repositories
+{
+    public class StockRepo : IEntityRepo<Stock>
+    { //TODO: implement Repo for Stock
+        public void Add(Stock entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Stock> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Stock? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, Stock entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PetShop/PetShop/PetShop.EF/Repositories/TransactionLinesRepo.cs
@@ -0,0 +1,37 @@
+﻿using PetShop.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.EF.Repositories
+{
+    public class TransactionLinesRepo : IEntityRepo<TransactionLine>
+    {//TODO: implement TransacionLinesRepo
+        public void Add(TransactionLine entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<TransactionLine> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TransactionLine? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, TransactionLine entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
